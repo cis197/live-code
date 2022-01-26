c5d0b6fdd7c627580e5c7a9c9e618a7cead5be51
printer83mph <printer.83mph@gmail.com>
2022-01-26 10:23:13 -0500

    Initial lecture4 commit

---
+++ S2022/lecture4/3.react/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    semi: ['error', 'never'],
+
+    'react/function-component-definition': ['error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+  },
+}
---
+++ S2022/lecture4/3.react/src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// eslint-disable-next-line import/no-unresolved
+import styles from './styles/main.module.css'
+
+const App = () => (
+  <main className={styles.container}>
+    Hello World!
+  </main>
+)
+
+export default App
