5b255fb3338b35cab38e605cc8978a6480cca0c2
printer83mph <printer.83mph@gmail.com>
2022-02-01 18:14:49 -0500

    Add tailwind

--- S2022/lecture4/3.react/src/components/post-preview.jsx
+++ S2022/lecture4/3.react/src/components/post-preview.jsx
@@ -17,6 +17,23 @@ const PostPreview = ({ title, description }) => (
   </article>
 )
 
+// tailwind??
+// const PostPreview = ({ title, description }) => (
+//   <article className="p-4 shadow-lg rounded-xl w-[400px]">
+//     <img
+//       src={imageUrl}
+//       alt="Swapneel"
+//       width="400"
+//       className="w-full rounded mb-4"
+//     />
+//     <h3 className="text-2xl mb-3">{title}</h3>
+//     <p className="mb-1">{description}</p>
+//     <a href="#more" className="text-gray-500 hover:underline">
+//       Read More
+//     </a>
+//   </article>
+// )
+
 // try inspect element and see what the class names end up as!
 // notice how we stop class name collision!
 
---
+++ S2022/lecture4/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['3.react/**/*.{html,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
