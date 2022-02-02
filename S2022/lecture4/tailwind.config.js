96dc724faf3484f328e7622e190d07b7d9a273cc
printer83mph <printer.83mph@gmail.com>
2022-02-02 14:46:26 -0500

    Tailwind theming stuff

--- S2022/lecture4/tailwind.config.js
+++ S2022/lecture4/tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ['3.react/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
+    // fontFamily: {
+    //   sans: ['Arial', 'Helvetica', 'sans-serif'],
+    // },
     extend: {},
   },
   plugins: [],
